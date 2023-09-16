603e69a318d9f0d2432bd5583d6e10e9cccf579f
ak28pujy <105053221+ak28pujy@users.noreply.github.com>
2023-09-16 21:24:15 +0200

    Outsourcing of prompts and logic changes for browser use

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import asyncio
 import os
 import platform
-import time
 import urllib.parse
 from concurrent.futures import ThreadPoolExecutor
 from datetime import date
 
 import feedparser
 import openai
-from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
@@ -19,6 +17,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
 import counttokens
+import openai_prompt
 import pdf
 
 OPENAI_API_KEY = 'OPENAI_API_KEY'
@@ -38,6 +37,8 @@ google_cse_id = os.getenv(GOOGLE_CSE_ID)
 semaphore = asyncio.Semaphore(int(os.getenv(MAX_CONCURRENT_TASKS, 3)))
 max_concurrent_urls = int(os.getenv(MAX_CONCURRENT_URLS, 5))
 page_load_timeout = int(os.getenv(PAGE_LOAD_TIMEOUT, 15))
+
+google_cookie_accepted = False
 
 
 async def limited_execute(task, *args):
@@ -65,25 +66,20 @@ BROWSERS = [{"driver": webdriver.Firefox, "path": get_driver_path("geckodriver")
 
 
 def get_browser(browser):
-    try:
-        if browser["driver"] == webdriver.Firefox:
-            firefox_options = webdriver.FirefoxOptions()
-            firefox_options.set_preference("pdfjs.disabled", False)
-            firefox_options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
-            firefox_options.set_preference("browser.download.manager.showWhenStarting", False)
-            firefox_options.set_preference("browser.download.panel.shown", False)
-            return browser["driver"](options=firefox_options,
-                                     service=browser["service"](executable_path=browser["path"], log_output=os.devnull))
-        else:
-            chrome_options = webdriver.ChromeOptions()
-            prefs = {"download.prompt_for_download": False, "download.directory_upgrade": True,
-                     "plugins.always_open_pdf_externally": False, "download.default_directory": "/dev/null"}
-            chrome_options.add_experimental_option('prefs', prefs)
-            return browser["driver"](options=chrome_options,
-                                     service=browser["service"](executable_path=browser["path"]))
-    except Exception as e:
-        print(f"Error with {browser['driver'].__name__}: {e}")
-        return None
+    if browser["driver"] == webdriver.Firefox:
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.set_preference("pdfjs.disabled", False)
+        firefox_options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+        firefox_options.set_preference("browser.download.manager.showWhenStarting", False)
+        firefox_options.set_preference("browser.download.panel.shown", False)
+        return browser["driver"](options=firefox_options,
+                                 service=browser["service"](executable_path=browser["path"], log_output=os.devnull))
+    else:
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {"download.prompt_for_download": False, "download.directory_upgrade": True,
+                 "plugins.always_open_pdf_externally": False, "download.default_directory": "/dev/null"}
+        chrome_options.add_experimental_option('prefs', prefs)
+        return browser["driver"](options=chrome_options, service=browser["service"](executable_path=browser["path"]))
 
 
 def google_search(search_term, api_key, cse_id, **kwargs):
@@ -128,20 +124,33 @@ async def execute_google_news_rss(search_terms, search_func, num_results):
 
 
 def execute_extract_text_from_url(url_list, browsers):
-    with ThreadPoolExecutor(max_workers=max_concurrent_urls) as executor:
-        return list(executor.map(lambda url: extract_text_from_url(url, browsers), url_list))
+    results = []
+    browser = None
+    for browser_info in browsers:
+        try:
+            browser = get_browser(browser_info)
+            with ThreadPoolExecutor(max_workers=max_concurrent_urls) as executor:
+                results.extend(list(executor.map(lambda url: extract_text_from_url(url, browser), url_list)))
+            break
+        except Exception as e:
+            print(f"Error using {browser_info['driver'].__name__}: {e}")
+            if browser:
+                browser.quit()
+                browser = None
+    if browser:
+        browser.quit()
+    return results
 
 
-def extract_text_from_url(url, browsers):
-    for browser in browsers:
-        browser = get_browser(browser)
-        page_content = get_page_content(browser, url)
-        if page_content:
-            return page_content
+def extract_text_from_url(url, browser):
+    page_content = get_page_content(browser, url)
+    if page_content:
+        return page_content
     return ""
 
 
 def get_page_content(browser, url):
+    global google_cookie_accepted
     page_text = ""
     if not browser:
         print("\nError: Browser instance is None")
@@ -149,102 +158,20 @@ def get_page_content(browser, url):
     try:
         browser.set_page_load_timeout(page_load_timeout)
         browser.get(url)
-        if '.google.com' in url:
+        if not google_cookie_accepted and '.google.com' in url:
             try:
                 browser.find_element(By.XPATH, "//.[@aria-label='Alle akzeptieren']").click()
-                time.sleep(5)
+                google_cookie_accepted = True
             except Exception as e:
                 print(f"\nError clicking the accept button on Google: {e}")
         page_text = BeautifulSoup(browser.page_source, 'html.parser').get_text()
     except Exception as e:
         print(f"\nError fetching the URL {url}: {e}")
-    finally:
-        browser.quit()
     return page_text
 
 
-async def execute_summarize_each_url(info_dict, company, model, language):
-    summarized_info = {}
-    async with ClientSession() as session:
-        openai.aiosession.set(session)
-        tasks = []
-        for search_term, info_url_list in info_dict.items():
-            summarized_info[search_term] = []
-            for info, url in info_url_list:
-                task = summarize_each_url(info, url, company, model, language)
-                tasks.append((task, search_term))
-        results = await asyncio.gather(*[t for t, _ in tasks])
-        for result, search_term in zip(results, [st for _, st in tasks]):
-            summarized_info[search_term].append(result)
-    await openai.aiosession.get().close()
-    return summarized_info
-
-
-async def summarize_each_url(info, url, company, model, language):
-    prompt = [{"role": "user",
-               "content": f"Bitte analysiere die folgende Webseite ({url}), auf der {company} erwähnt wird, und "
-                          "erstelle eine prägnante Zusammenfassung der relevanten Informationen."},
-              {"role": "user", "content": f"Inhalt der Webseite:\n\n{info}"},
-              {"role": "user", "content": "Fokussiere dich beim Extrahieren der Informationen auf klare, konkrete und "
-                                          "nützliche Details, und vermeide dabei irrelevante oder redundante Inhalte. "
-                                          f"Die Antwort sollte in {language} verfasst sein."}]
-    response = await openai.ChatCompletion.acreate(model=model, messages=prompt, temperature=1, top_p=1.0, n=1,
-                                                   frequency_penalty=0.0, presence_penalty=0.0)
-    print(f"\n{url}:\n", response['choices'][0]['message']['content'])
-    summary = response['choices'][0]['message']['content']
-    return summary, url
-
-
-def summarize(info_dict, company, model, language):
-    prompt = [{"role": "system",
-               "content": "Du bist ein kenntnisreicher KI-Assistent, spezialisiert auf Unternehmensanalysen."},
-              {"role": "user", "content": "Bitte erstelle eine strukturierte Übersicht für die Kundenakquise eines "
-                                          f"IT-Beratungsunternehmens, um {company} als Neukunden zu gewinnen. "
-                                          "Berücksichtige dabei die folgenden Aspekte und fasse im Anschluss "
-                                          "die Informationen in einem Fließtext zusammen:\n\n"
-                                          "1. **Allgemeine Unternehmensinformationen:**\n"
-                                          "   - Unternehmensname\n"
-                                          "   - Gründungsdatum\n"
-                                          "   - Gründer\n"
-                                          "   - Aktueller CEO\n"
-                                          "   - Hauptsitz\n"
-                                          "   - Branche\n"
-                                          "   - Website des Unternehmens\n\n"
-                                          "2. **Produkt- und Dienstleistungsportfolio:**\n"
-                                          "   - Hauptprodukte und -dienstleistungen\n"
-                                          "   - USP oder Alleinstellungsmerkmale\n"
-                                          "   - Aussagen von Branchenexperten, Journalisten oder "
-                                          "anderen relevante Dritten\n\n"
-                                          "3. **Organisation und Mitarbeiter:**\n"
-                                          "   - Anzahl der Mitarbeiter\n"
-                                          "   - Organisationsstruktur\n\n"
-                                          "4. **Finanzielle und Marktinformationen:**\n"
-                                          "   - Geschäftszahlen\n"
-                                          "   - Marktposition\n\n"
-                                          "5. **Unternehmensstrategie, -vision und -reputation:**\n"
-                                          "   - Unternehmensmission und -vision\n"
-                                          "   - Geschäftliche Höhepunkte\n"
-                                          "   - Zukunftsprojekte\n"
-                                          "   - Soziale Verantwortung\n"
-                                          "   - Auszeichnungen und Anerkennungen\n\n"}]
-    for search_term, info_url_list in info_dict.items():
-        for info, url in info_url_list:
-            prompt.append(
-                {"role": "user", "content": f"Hier sind zusätzliche Informationen, die ich auf einer Webseite ({url}) "
-                                            f"gefunden habe, welche {company} erwähnt. Diese wurden bereits von "
-                                            f"der OpenAI API in einer anderen Anfrage verarbeitet: {info}"})
-    prompt.append({"role": "user", "content": "Integriere bitte nur die relevanten Informationen bezüglich der "
-                                              f"oben genannten Aspekte in den Steckbrief von {company}. Sollten einige "
-                                              "Informationen fehlen, nutze dein allgemeines Wissen, wenn es sinnvoll "
-                                              "und zutreffend ist, ansonsten lass das betreffende Feld leer. "
-                                              f"Gehe dabei systematisch vor und antworte bitte in {language}."})
-    response = openai.ChatCompletion.create(model=model, messages=prompt, temperature=1, top_p=1.0, n=1,
-                                            frequency_penalty=0.0, presence_penalty=0.0)
-    return response['choices'][0]['message']['content'], prompt, response
-
-
 def generate_report(info_dict, company, model, language, prompt_as_txt, report_as_txt, report_as_pdf):
-    response_content, prompt, response = summarize(info_dict, company, model, language)
+    response_content, prompt, response = openai_prompt.summarize(info_dict, company, model, language)
     print(f"\nModel: {model}")
     print(
         f"\n{counttokens.num_tokens_from_messages(prompt, model)} prompt tokens counted by num_tokens_from_messages().")
@@ -313,19 +240,19 @@ async def main(company, search_terms_google_search, search_terms_google_news, mo
     for search_term, info_url_list in info_dict_all.items():
         print(f"\nFound URL(s) for the search term '{search_term}': {', '.join(url for info, url in info_url_list)}")
 
-    info_dict_all = await execute_summarize_each_url(info_dict_all, company, model, language)
+    info_dict_all = await openai_prompt.execute_summarize_each_url(info_dict_all, company, model, language)
 
     generate_report(info_dict_all, company, model, language, prompt_as_txt, report_as_txt, report_as_pdf)
 
 
 if __name__ == "__main__":
-    company_1 = "NOW GMBH"
+    company_1 = "Apple"
     search_terms_google_search_2 = [company_1]
     search_terms_google_news_3 = [company_1]
     model_4 = "gpt-3.5-turbo-16k"  # "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"
     language_5 = "German"  # "English", "German", "French"
-    num_urls_google_search_6 = 10
-    num_urls_google_news_7 = 10
+    num_urls_google_search_6 = 3
+    num_urls_google_news_7 = 3
     prompt_as_txt_8 = True
     report_as_txt_9 = True
     report_as_pdf_10 = True
---
+++ openai_prompt.py
@@ -0,0 +1,84 @@
+import asyncio
+
+import openai
+from aiohttp import ClientSession
+
+
+async def execute_summarize_each_url(info_dict, company, model, language):
+    summarized_info = {}
+    async with ClientSession() as session:
+        openai.aiosession.set(session)
+        tasks = []
+        for search_term, info_url_list in info_dict.items():
+            summarized_info[search_term] = []
+            for info, url in info_url_list:
+                task = summarize_each_url(info, url, company, model, language)
+                tasks.append((task, search_term))
+        results = await asyncio.gather(*[t for t, _ in tasks])
+        for result, search_term in zip(results, [st for _, st in tasks]):
+            summarized_info[search_term].append(result)
+    await openai.aiosession.get().close()
+    return summarized_info
+
+
+async def summarize_each_url(info, url, company, model, language):
+    prompt = [{"role": "user",
+               "content": f"Bitte analysiere die folgende Webseite ({url}), auf der {company} erwähnt wird, und "
+                          "erstelle eine prägnante Zusammenfassung der relevanten Informationen."},
+              {"role": "user", "content": f"Inhalt der Webseite:\n\n{info}"},
+              {"role": "user", "content": "Fokussiere dich beim Extrahieren der Informationen auf klare, konkrete und "
+                                          "nützliche Details, und vermeide dabei irrelevante oder redundante Inhalte. "
+                                          f"Die Antwort sollte in {language} verfasst sein."}]
+    response = await openai.ChatCompletion.acreate(model=model, messages=prompt, temperature=1, top_p=1.0, n=1,
+                                                   frequency_penalty=0.0, presence_penalty=0.0)
+    print(f"\n{url}:\n", response['choices'][0]['message']['content'])
+    summary = response['choices'][0]['message']['content']
+    return summary, url
+
+
+def summarize(info_dict, company, model, language):
+    prompt = [{"role": "system",
+               "content": "Du bist ein kenntnisreicher KI-Assistent, spezialisiert auf Unternehmensanalysen."},
+              {"role": "user", "content": "Bitte erstelle eine strukturierte Übersicht für die Kundenakquise eines "
+                                          f"IT-Beratungsunternehmens, um {company} als Neukunden zu gewinnen. "
+                                          "Berücksichtige dabei die folgenden Aspekte und fasse im Anschluss "
+                                          "die Informationen in einem Fließtext zusammen:\n\n"
+                                          "1. **Allgemeine Unternehmensinformationen:**\n"
+                                          "   - Unternehmensname\n"
+                                          "   - Gründungsdatum\n"
+                                          "   - Gründer\n"
+                                          "   - Aktueller CEO\n"
+                                          "   - Hauptsitz\n"
+                                          "   - Branche\n"
+                                          "   - Website des Unternehmens\n\n"
+                                          "2. **Produkt- und Dienstleistungsportfolio:**\n"
+                                          "   - Hauptprodukte und -dienstleistungen\n"
+                                          "   - USP oder Alleinstellungsmerkmale\n"
+                                          "   - Aussagen von Branchenexperten, Journalisten oder "
+                                          "anderen relevante Dritten\n\n"
+                                          "3. **Organisation und Mitarbeiter:**\n"
+                                          "   - Anzahl der Mitarbeiter\n"
+                                          "   - Organisationsstruktur\n\n"
+                                          "4. **Finanzielle und Marktinformationen:**\n"
+                                          "   - Geschäftszahlen\n"
+                                          "   - Marktposition\n\n"
+                                          "5. **Unternehmensstrategie, -vision und -reputation:**\n"
+                                          "   - Unternehmensmission und -vision\n"
+                                          "   - Geschäftliche Höhepunkte\n"
+                                          "   - Zukunftsprojekte\n"
+                                          "   - Soziale Verantwortung\n"
+                                          "   - Auszeichnungen und Anerkennungen\n\n"}]
+    for search_term, info_url_list in info_dict.items():
+        for info, url in info_url_list:
+            prompt.append(
+                {"role": "user", "content": f"Hier sind zusätzliche Informationen, die ich auf einer Webseite ({url}) "
+                                            f"gefunden habe, welche {company} erwähnt. Diese wurden bereits von "
+                                            f"der OpenAI API in einer anderen Anfrage verarbeitet: {info}"})
+    prompt.append({"role": "user", "content": "Integriere bitte nur die relevanten Informationen bezüglich der "
+                                              f"oben genannten Aspekte in den Steckbrief von {company}. Sollten einige "
+                                              "Informationen fehlen, nutze dein allgemeines Wissen, wenn es sinnvoll "
+                                              "und zutreffend ist, ansonsten lass das betreffende Feld leer. "
+                                              f"Gehe dabei systematisch vor und antworte bitte in {language}."})
+    response = openai.ChatCompletion.create(model=model, messages=prompt, temperature=1, top_p=1.0, n=1,
+                                            frequency_penalty=0.0, presence_penalty=0.0)
+    return response['choices'][0]['message']['content'], prompt, response
